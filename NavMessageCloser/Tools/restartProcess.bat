4727bae672cf418326512fb466f63316ad684dc3
dadahanov-mr <dadakhanov@gmail.com>
2021-09-04 11:51:34 +0300

    fix spaces increasing bug

--- NavMessageCloser/Tools/restartProcess.bat
+++ NavMessageCloser/Tools/restartProcess.bat
@@ -3,6 +3,7 @@ if "%1" == "" goto endx
 wmic process where handle=%1 get commandline /VALUE|FIND "CommandLine" >proccmd.txt
 set /P cmd=<proccmd.txt
 taskkill /PID %1 /F
+set cmd=%cmd:  = %
 echo %cmd:~12%>>restartprocess.log
 cmd.exe /C "%cmd:~12%"
 
